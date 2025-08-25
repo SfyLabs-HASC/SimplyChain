028e52555ee8cc9eab438b40ada7b81b154c6089
SfyLabs-HASC <sfy.startup@gmail.com>
2025-08-25 12:45:13 +0200

    rr

--- src/pages/QRCodePage.tsx
+++ src/pages/QRCodePage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-// Icona per il download (SVG inlined)
+// Icona per il download (SVG inlined))
 const DownloadIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
